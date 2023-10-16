3cefef1971acb0db292cfc88316a4d44a59bf473
ismailnamdar <namdari@itu.edu.tr>
2023-10-17 01:40:35 +0300

    add cors headers

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -17,6 +17,14 @@ export default async function handleRequest(
 ) {
   addDocumentResponseHeaders(request, responseHeaders);
 
+  const allowCorsHeaders = [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  ];
+  allowCorsHeaders.forEach(({ key, value }) => responseHeaders.set(key, value));
+
   const callbackName = isbot(request.headers.get("user-agent"))
     ? "onAllReady"
     : "onShellReady";
